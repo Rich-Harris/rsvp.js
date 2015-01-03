149b337fe5767dbb855e46137d8611d2fc63786f
Rich Harris <richard.a.harris@gmail.com>
2015-01-03 11:11:49 -0500

    try gobble and esperanto instead of broccoli/es6-module-transpiler

---
+++ gobblefile.js
@@ -0,0 +1,44 @@
+var gobble = require( 'gobble' ),
+	version = require( 'git-repo-version' ),
+	lib,
+	tests;
+
+// TODO add license banner
+lib = gobble( 'lib' ).transform( 'esperanto-bundle', {
+	entry: 'rsvp',
+	type: 'umd',
+	name: 'RSVP',
+	strict: true
+});
+
+tests = gobble([
+	// the test suite plus dependencies
+	gobble([ lib, 'test' ]).transform( 'browserify', {
+		entries: ['./index.js'],
+		dest: 'browserify.js'
+	}),
+
+	// rsvp.js (required by web worker test)
+	lib,
+
+	// static files
+	gobble( 'test' ).include( '{index.html,worker.js}' ),
+
+	// json3
+	gobble( 'node_modules/json3/lib' ).include( 'json3.js' ),
+
+	// mocha
+	gobble( 'node_modules/mocha' ).include( 'mocha.{js,css}' )
+]).moveTo( 'test' );
+
+// minify if building, rather than serving
+if ( gobble.env() === 'production' ) {
+	lib = gobble([
+		lib,
+		lib.transform( 'uglifyjs', {
+			ext: '.min.js'
+		})
+	]).transform( 'sorcery' ); // collapse sourcemaps
+}
+
+module.exports = gobble([ lib, tests ]);
